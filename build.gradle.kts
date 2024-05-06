f435bc5ab4218cf75bf3bde3c2f362efd220dc54
Yeonwoo Kim <kywoopid@gmail.com>
2024-05-06 18:00:53 +0900

    netty 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("io.netty:netty-all:4.0.31.Final")
 }
 
 tasks.test {
